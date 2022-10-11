fa43d88589bf42e60b98409ebbb864fa6f5347e1
zafyyre <zaaf17@hotmail.com>
2022-10-11 12:08:01 -0700

    Initial commit

---
+++ app/src/vacation-approval/components/NoVacations.js
@@ -0,0 +1,11 @@
+const noVacations = () => {
+  return (
+    <div className="approval-no-vacations">
+      <p>
+        All vacations have been processed.
+      </p>
+    </div>
+  )
+}
+
+export default noVacations
